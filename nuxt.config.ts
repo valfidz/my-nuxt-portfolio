94d99dfd0185dfaebc149b4927565ecd35aeeb38
Hoshiyomi Noparu <nugrahanaufalhafizh@gmail.com>
2026-07-01 10:32:16 +0700

    fix: update metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  app: {
+    head: {
+      title: 'VALCODE',
+      titleTemplate: '%s %separator VALCODE'
+    }
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/image', '@nuxtjs/supabase'],
   css: ['./app/assets/css/main.css'],
